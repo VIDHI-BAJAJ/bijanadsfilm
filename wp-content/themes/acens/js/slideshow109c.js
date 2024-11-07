2aea2bc61b04d71c854b3fcef4be1822d724c61b
Vidhi <vidhibajaj1401@gmail.com>
2024-11-07 17:25:18 +0530

    first commit

---
+++ wp-content/themes/acens/js/slideshow109c.js
@@ -0,0 +1,17 @@
+// jQuery(document).ready(function() {
+//     jQuery('#kenburnsSliderContainer').vegas({
+//         slides: [{
+//             src: "https://shtheme.com/demosd/acenswp/wp-content/uploads/2023/12/2-3.jpg"
+//         }, {
+//             src: "https://shtheme.com/demosd/acenswp/wp-content/uploads/2023/12/6-2.jpg"
+//         }, {
+//             src: "https://shtheme.com/demosd/acenswp/wp-content/uploads/2023/12/5-2.jpg"
+//         }],
+//         overlay: true,
+//         transition: 'fade2',
+//         animation: 'kenburnsUpRight',
+//         transitionDuration: 1000,
+//         delay: 10000,
+//         animationDuration: 20000
+//     });
+// });
